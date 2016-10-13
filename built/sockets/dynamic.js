ca213c40f3a27b6738c0e5b3dddff2caefa99f7c
echo-peak <echopeakdev@gmail.com>
2016-10-13 04:12:20 -0400

    removed config

--- built/sockets/dynamic.js
+++ built/sockets/dynamic.js
@@ -3,7 +3,7 @@ let electron = require('electron');
 let path = require('path');
 let child_process = require('child_process');
 let stream = require('stream');
-let CONFIG = require('../DB/config');
+
 
 //interfaces with dynamic browser window only
 module.exports = class socket_dynamic{
