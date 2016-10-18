be17d8bb005e524627d3371166dc36578fefb358
echo-peak <echopeakdev@gmail.com>
2016-10-18 18:09:57 -0400

    added mobx stores for youtube, videos, pictures

--- src/admin/store/index.jsx
+++ src/admin/store/index.jsx
@@ -2,10 +2,16 @@ import x_audio from './audio';
 import x_browserWindow from './browser-window-config';
 import x_shell from './shell';
 import x_window from './windows';
+import x_youtube from './youtube';
+import x_videos from './videos';
+import x_pictures from './pictures';
 
 export let Stores = {
   audio:new x_audio(),
   browserWindow:new x_browserWindow(),
   shell:new x_shell(),
-  window:new x_window()
+  window:new x_window(),
+  youtube:new x_youtube(),
+  videos:new x_videos(),
+  pictures:new x_pictures(),
 }
