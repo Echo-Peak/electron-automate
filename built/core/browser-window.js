dd39ed48af0a9c8c9a6aec6c058e8d66225a9601
echo-peak <echopeakdev@gmail.com>
2016-10-21 14:17:49 -0400

    preparing for rewrite

--- built/core/browser-window.js
+++ built/core/browser-window.js
@@ -1,41 +1,22 @@
 let electron = require('electron');
 
 //refrence to BrowserWindow_delagate , config to be used on 'BrowserWindow_delagate' at runtime
-module.exports  = function(browserWindow ,socket, config){
-  let screen = electron.screen.getPrimaryDisplay().workArea;
-  let currentDims = {};
 
-  socket.on('window-pos' ,function(pos){
-    /* pos {
-    x:<int>
-    y:<int>
-    auto:<boolean>
-    scale:<int>
+
+class BrowserWindow_config{
+  constructor(window , socket, Sockets){
+    this.window = window;
+    this.Sockets = Sockets;
+    this.socket = socket;
+
   }
-    */
-    browserWindow.setBounds({x:pos.x, y:pos.y, width:currentDims.width,height:currentDims.height});
-  });
+  update(prop){
+    //window properties that can be changed at runtime
+    switch(prop){
 
-  socket.on('window-dims' ,function(dims){
-    currentDims.width = dims.width;
-    currentDims.height = dims.height;
-    browserWindow.setSize(dims.width, dims.height);
-  });
-
-  socket.on('window-timeout' ,function(delay){
-    setTimeout(function(){
-      browserWindow.hide();
-    },delay || 1000)
-  });
-
-  socket.on('window-toggle' ,function(bool){
-    if(bool){
-      browserWindow.show();
-    }else{
-      browserWindow.hide();
     }
-  });
-  socket.on('window-settings' ,function(settings){
 
-  });
+
+  }
 }
+module.exports = BrowserWindow_config
